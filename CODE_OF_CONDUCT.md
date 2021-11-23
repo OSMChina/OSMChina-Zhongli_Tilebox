8fda0993e0543daded3d93657916c0bd26155f62
LaoshuBaby <keaitianxinmail@qq.com>
2021-11-23 21:11:44 +0800

    按照@AustinZhu的建议转换为下划线命名风格
    Co-Authored-By: Austin Zhu <42071208+AustinZhu@users.noreply.github.com>

---
+++ CODE_OF_CONDUCT.md
@@ -0,0 +1,9 @@
+# CODE_OF_CONDUCT
+
+本项目沿用OSMChina的全局[代码规范](https://github.com/OSMChina/OSMChina-CODE_OF_CONDUCT)。
+
+未尽事宜按照本项目中的局部代码规范补充。若有冲突之处，以局部代码规范为准。
+
+----------
+
+1. 命名应参考PEP8
--- Src/Requester.py
+++ Src/Requester.py
@@ -35,8 +35,8 @@ headers = {
 
 
 def RandomChar(begin: str, end: str):
-    Range = int(ord(end) - ord(begin))
-    tmp = random.randint(0, Range - 1)
+    range = int(ord(end) - ord(begin))
+    tmp = random.randint(0, range - 1)
     return chr(ord(begin) + tmp)
 
 
@@ -46,49 +46,49 @@ def fullURL(x: int, y: int, z: int, tile_name):
     PROTOCOL_PREFIX_HTTP = "http://"
     PROTOCOL_PREFIX_FTP = "ftp://"
     # 开始组装准备
-    URL = TILE_SERVER[tile_name][0]
+    url = TILE_SERVER[tile_name][0]
     # 检查URL是否合法
     for i in WHITE_LIST:
-        if i in URL:
+        if i in url:
             break
     else:
         print("Error: Not OSMChina tile service!")
     # 复杂替换预配置
-    Protocol_list = TILE_SERVER[tile_name][1]
+    protocol_list = TILE_SERVER[tile_name][1]
     if TILE_SERVER[tile_name][2] != "":
-        Random_list = [TILE_SERVER[tile_name][2].split(
+        random_list = [TILE_SERVER[tile_name][2].split(
             "-")[0], TILE_SERVER[tile_name][2].split("-")[1]]
     else:
-        Random_list = ""
+        random_list = ""
     # 组装协议
-    if Protocol_list[0] == "https":
-        URL = URL.replace("{protocol}", PROTOCOL_PREFIX_HTTPS)
-    elif Protocol_list[0] == "ftp":
-        URL = URL.replace("{protocol}", PROTOCOL_PREFIX_FTP)
+    if protocol_list[0] == "https":
+        url = url.replace("{protocol}", PROTOCOL_PREFIX_HTTPS)
+    elif protocol_list[0] == "ftp":
+        url = url.replace("{protocol}", PROTOCOL_PREFIX_FTP)
     else:
-        URL = URL.replace("{protocol}", PROTOCOL_PREFIX_HTTP)
+        url = url.replace("{protocol}", PROTOCOL_PREFIX_HTTP)
     # 组装负载均衡
-    if Random_list != "":
-        URL = URL.replace("{random}", RandomChar(
-            Random_list[0], Random_list[1]) + ".")
+    if random_list != "":
+        url = url.replace("{random}", RandomChar(
+            random_list[0], random_list[1]) + ".")
     else:
-        URL = URL.replace("{random}", "")
+        url = url.replace("{random}", "")
     # 组装瓦片坐标
-    URL = URL.replace("{x}", str(x))
-    URL = URL.replace("{y}", str(y))
-    URL = URL.replace("{z}", str(z))
+    url = url.replace("{x}", str(x))
+    url = url.replace("{y}", str(y))
+    url = url.replace("{z}", str(z))
     # 组装Retina分辨率 优先最大分辨率
     if TILE_SERVER[tile_name][3][0] != "":
-        URL = URL.replace(
+        url = url.replace(
             "{retina}", "@" + TILE_SERVER[tile_name][3][len(TILE_SERVER[tile_name][3]) - 1] + "x")
     else:
-        URL = URL.replace("{retina}", "")
+        url = url.replace("{retina}", "")
     # 组装APIKEY
     if TILE_SERVER[tile_name][4] != "":
-        URL = URL.replace("{apikey}", TILE_SERVER[tile_name][4])
+        url = url.replace("{apikey}", TILE_SERVER[tile_name][4])
     else:
-        URL = URL.replace("{apikey}", "")
-    return URL
+        url = url.replace("{apikey}", "")
+    return url
 
 
 class singleTileTask(threading.Thread):
@@ -112,27 +112,27 @@ class singleTileTask(threading.Thread):
 
     def run(self):
         try:
-            URL = fullURL(self.x, self.y, self.z, self.tile_name)
-            IMG = requests.get(URL, headers=headers)
+            url = fullURL(self.x, self.y, self.z, self.tile_name)
+            img = requests.get(url, headers=headers)
             filename = str(self.y) + ".png"
             with open(filename, "wb") as f:
-                f.write(IMG.content)
-            if IMG.status_code == 200:
-                print("[Thread " + str(self.threadID) + "][+] " + URL)
+                f.write(img.content)
+            if img.status_code == 200:
+                print("[Thread " + str(self.threadID) + "][+] " + url)
             else:
-                print("[Thread " + str(self.threadID) + "][-] " + URL)
+                print("[Thread " + str(self.threadID) + "][-] " + url)
         except Exception as e:
             print(e)
         exit(0)
 
 
 def atomicTask(x: int, y: int, z: int, tile_name: str):
-    TaskURL = fullURL(x, y, z, tile_name)
-    img = requests.get(url=TaskURL, headers=headers)
+    url = fullURL(x, y, z, tile_name)
+    img = requests.get(url=url, headers=headers)
     filename = str(y) + ".png"
     with open(filename, "wb") as f:
         f.write(img.content)
-    print("[Thread 0][*] " + TaskURL)
+    print("[Thread 0][*] " + url)
 
 
 def multipleTask(x_min, x_max, y_min, y_max, z, tile_name, task_name, ALLOW_MP=False):
@@ -141,7 +141,7 @@ def multipleTask(x_min, x_max, y_min, y_max, z, tile_name, task_name, ALLOW_MP=F
     os.mkdir(task_name)
     os.chdir(task_name)
 
-    TIME_START = time.time()
+    time_start = time.time()
 
     # TASKBODY
     for x in range(x_min, x_max):
@@ -166,8 +166,8 @@ def multipleTask(x_min, x_max, y_min, y_max, z, tile_name, task_name, ALLOW_MP=F
         os.chdir("..")
     os.chdir("..")
 
-    TIME_END = time.time()
-    print("[TIME] " + task_name + ": " + str(TIME_END - TIME_START) + "s")
+    time_end = time.time()
+    print("[TIME] " + task_name + ": " + str(time_end - time_start) + "s")
 
 
 def taskGenerator(zoom: int, tile_name, task_name, x_min=0, x_max=0, y_min=0, y_max=0, grid_pos=[0, 0],
@@ -194,18 +194,18 @@ def taskGenerator(zoom: int, tile_name, task_name, x_min=0, x_max=0, y_min=0, y_
                 x_max = int(input("Please input x_max:"))
                 y_min = int(input("Please input y_min:"))
                 y_max = int(input("Please input y_max:"))
-            Count = (x_max - x_min + 1) * (y_max - y_min + 1)
-            print("Total tiles:", Count)
+            count = (x_max - x_min + 1) * (y_max - y_min + 1)
+            print("Total tiles:", count)
             multipleTask(x_min, x_max, y_min, y_max, zoom,
                          tile_name, task_name, ALLOW_MP)
     elif MODE == "Full":
         if zoom == 0:
-            Count = 1
-            print("Total tiles:", Count)
+            count = 1
+            print("Total tiles:", count)
             multipleTask(0, 0, 0, 0, 0, tile_name, task_name)
         else:
-            Count = pow(2, zoom * 2)
-            print("Total tiles:", Count)
+            count = pow(2, zoom * 2)
+            print("Total tiles:", count)
             multipleTask(0, pow(2, zoom) - 1, 0, pow(2, zoom) -
                          1, zoom, tile_name, task_name, ALLOW_MP)
     elif MODE == "Grid":
@@ -231,8 +231,8 @@ def taskGenerator(zoom: int, tile_name, task_name, x_min=0, x_max=0, y_min=0, y_
         print("zoom:", zoom)
         print("grid_zoom:", grid_zoom)
         print("grid_number:", grid_number)
-        Count = pow(2, zoom * 2)
-        print("Total tiles:", Count)
+        count = pow(2, zoom * 2)
+        print("Total tiles:", count)
         print("===")
     else:
         print("Error: MODE Error")
