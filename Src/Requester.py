41b22aca252e5d2a5fe39072424f36fc7ce67b2a
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-11-23 18:58:42 +0900

    fmt: reformat code and project structure

--- Src/Requester.py
+++ Src/Requester.py
@@ -11,20 +11,22 @@ import requests
 global WHITE_LIST
 global TILE_SERVER
 
+
 def UA():
-    PROGRAME_NAME="OSMChina-TileRequest"
-    PROGRAME_VERSION="0.3.0"
-    PLATFORM_SYSTEM=platform.system()
-    PLATFORM_VERSION=platform.version()
-    PLATFORM_MACHINE=platform.machine()
-    PLATFORM_PYTHON=platform.python_version()
-    UA_BASIC= PROGRAME_NAME+"/"+PROGRAME_VERSION
-    UA_EXTEND="("\
-              +PLATFORM_SYSTEM+" "+PLATFORM_VERSION+"; "\
-              +PLATFORM_MACHINE+"; "\
-              +")"\
-              +" Python/"+PLATFORM_PYTHON
+    PROGRAME_NAME = "OSMChina-TileRequest"
+    PROGRAME_VERSION = "0.3.0"
+    PLATFORM_SYSTEM = platform.system()
+    PLATFORM_VERSION = platform.version()
+    PLATFORM_MACHINE = platform.machine()
+    PLATFORM_PYTHON = platform.python_version()
+    UA_BASIC = PROGRAME_NAME+"/"+PROGRAME_VERSION
+    UA_EXTEND = "("\
+        + PLATFORM_SYSTEM+" "+PLATFORM_VERSION+"; "\
+        + PLATFORM_MACHINE+"; "\
+        + ")"\
+        + " Python/"+PLATFORM_PYTHON
     return UA_BASIC+" "+UA_EXTEND
+
 
 headers = {
     "User-Agent": "OSMChina-TileRequest/0.3.0",
@@ -43,42 +45,45 @@ def fullURL(x: int, y: int, z: int, tile_name):
     PROTOCOL_PREFIX_HTTPS = "https://"
     PROTOCOL_PREFIX_HTTP = "http://"
     PROTOCOL_PREFIX_FTP = "ftp://"
-    # 开始组装准备
+    # ?????????
     URL = TILE_SERVER[tile_name][0]
-    # 检查URL是否合法
+    # ???URL?????
     for i in WHITE_LIST:
         if i in URL:
             break
     else:
         print("Error: Not OSMChina tile service!")
-    # 复杂替换预配置
+    # ?????滻?????
     Protocol_list = TILE_SERVER[tile_name][1]
     if TILE_SERVER[tile_name][2] != "":
-        Random_list = [TILE_SERVER[tile_name][2].split("-")[0], TILE_SERVER[tile_name][2].split("-")[1]]
+        Random_list = [TILE_SERVER[tile_name][2].split(
+            "-")[0], TILE_SERVER[tile_name][2].split("-")[1]]
     else:
         Random_list = ""
-    # 组装协议
+    # ???Э??
     if Protocol_list[0] == "https":
         URL = URL.replace("{protocol}", PROTOCOL_PREFIX_HTTPS)
     elif Protocol_list[0] == "ftp":
         URL = URL.replace("{protocol}", PROTOCOL_PREFIX_FTP)
     else:
         URL = URL.replace("{protocol}", PROTOCOL_PREFIX_HTTP)
-    # 组装负载均衡
+    # ??????????
     if Random_list != "":
-        URL = URL.replace("{random}", RandomChar(Random_list[0], Random_list[1]) + ".")
+        URL = URL.replace("{random}", RandomChar(
+            Random_list[0], Random_list[1]) + ".")
     else:
         URL = URL.replace("{random}", "")
-    # 组装瓦片坐标
+    # ??????????
     URL = URL.replace("{x}", str(x))
     URL = URL.replace("{y}", str(y))
     URL = URL.replace("{z}", str(z))
-    # 组装Retina分辨率 优先最大分辨率
+    # ???Retina????? ???????????
     if TILE_SERVER[tile_name][3][0] != "":
-        URL = URL.replace("{retina}", "@" + TILE_SERVER[tile_name][3][len(TILE_SERVER[tile_name][3]) - 1] + "x")
+        URL = URL.replace(
+            "{retina}", "@" + TILE_SERVER[tile_name][3][len(TILE_SERVER[tile_name][3]) - 1] + "x")
     else:
         URL = URL.replace("{retina}", "")
-    # 组装APIKEY
+    # ???APIKEY
     if TILE_SERVER[tile_name][4] != "":
         URL = URL.replace("{apikey}", TILE_SERVER[tile_name][4])
     else:
@@ -106,13 +111,13 @@ class singleTileTask(threading.Thread):
         self.threadID = threadID
 
     def run(self):
-        # 开始请求 下面这段全是copilot干的好事
+        # ??????? ??????????copilot??????
         try:
             URL = fullURL(self.x, self.y, self.z, self.tile_name)
             IMG = requests.get(URL, headers=headers)
             filename = str(self.y) + ".png"
 
-            # # 修正子进程目录
+            # # ???????????
             # PWD=os.getcwd()
             # # print("PWD:", PWD)
             # import platform
@@ -155,14 +160,14 @@ def atomicTask(x: int, y: int, z: int, tile_name: str):
 def multipleTask(x_min, x_max, y_min, y_max, z, tile_name, task_name, ALLOW_MP=False):
     x_max += 1
     y_max += 1
-    os.system("mkdir " + task_name)
+    os.mkdir(task_name)
     os.chdir(task_name)
 
     TIME_START = time.time()
 
     # TASKBODY
     for x in range(x_min, x_max):
-        os.system("mkdir " + str(x))
+        os.mkdir(str(x))
         os.chdir(str(x))
         if ALLOW_MP == False:
             for y in range(y_min, y_max):
@@ -187,17 +192,19 @@ def multipleTask(x_min, x_max, y_min, y_max, z, tile_name, task_name, ALLOW_MP=F
     print("[TIME] " + task_name + ": " + str(TIME_END - TIME_START) + "s")
 
 
-def taskGenerator(zoom:int, tile_name, task_name, x_min=0, x_max=0, y_min=0, y_max=0, grid_pos=[0,0],
+def taskGenerator(zoom: int, tile_name, task_name, x_min=0, x_max=0, y_min=0, y_max=0, grid_pos=[0, 0],
                   MODE="Region", ALLOW_MP=False):
     # INIT
     global WHITE_LIST
     global TILE_SERVER
-    headers["User-Agent"]=UA()
+    headers["User-Agent"] = UA()
     if platform.system() == "Windows":
-        WHITE_LIST = json.loads(open('..\\Res\\control_list.json').read())['WHITE_LIST']
+        WHITE_LIST = json.loads(open('..\\Res\\control_list.json').read())[
+            'WHITE_LIST']
         TILE_SERVER = json.loads(open('..\\Res\\tile_server.json').read())
     else:
-        WHITE_LIST = json.loads(open('../Res/control_list.json').read())['WHITE_LIST']
+        WHITE_LIST = json.loads(
+            open('../Res/control_list.json').read())['WHITE_LIST']
         TILE_SERVER = json.loads(open('../Res/tile_server.json').read())
     # TASK
     if MODE == "Region":
@@ -211,7 +218,8 @@ def taskGenerator(zoom:int, tile_name, task_name, x_min=0, x_max=0, y_min=0, y_m
                 y_max = int(input("Please input y_max:"))
             Count = (x_max - x_min + 1) * (y_max - y_min + 1)
             print("Total tiles:", Count)
-            multipleTask(x_min, x_max, y_min, y_max, zoom, tile_name, task_name, ALLOW_MP)
+            multipleTask(x_min, x_max, y_min, y_max, zoom,
+                         tile_name, task_name, ALLOW_MP)
     elif MODE == "Full":
         if zoom == 0:
             Count = 1
@@ -220,29 +228,31 @@ def taskGenerator(zoom:int, tile_name, task_name, x_min=0, x_max=0, y_min=0, y_m
         else:
             Count = pow(2, zoom * 2)
             print("Total tiles:", Count)
-            multipleTask(0, pow(2, zoom) - 1, 0, pow(2, zoom) - 1, zoom, tile_name, task_name, ALLOW_MP)
+            multipleTask(0, pow(2, zoom) - 1, 0, pow(2, zoom) -
+                         1, zoom, tile_name, task_name, ALLOW_MP)
     elif MODE == "Grid":
-        def findNearstPow2(x:int):
+        def findNearstPow2(x: int):
             for i in range(19):
                 if pow(2, i) >= x:
-                    return pow(2,i)
-        tolerance_zoom=7
+                    return pow(2, i)
+        tolerance_zoom = 7
         grid_zoom = int(pow(2, int(zoom - tolerance_zoom+1)))//2
-        if zoom>=10:
-            grid_zoom=findNearstPow2(int(sqrt(int(pow(3, int(zoom - tolerance_zoom+1)))//1.5)))
-        if grid_zoom<=0:
-            grid_zoom=0
+        if zoom >= 10:
+            grid_zoom = findNearstPow2(
+                int(sqrt(int(pow(3, int(zoom - tolerance_zoom+1)))//1.5)))
+        if grid_zoom <= 0:
+            grid_zoom = 0
         if grid_zoom <= 1:
             grid_number = int(pow(2, grid_zoom//2))
-        elif zoom>=10:
+        elif zoom >= 10:
             grid_number = int(pow(2, grid_zoom//2))
         else:
             grid_number = int(pow(2, grid_zoom))
-        if grid_number >65536:
-            grid_number = 65536*pow(2,findNearstPow2(zoom-tolerance_zoom-3))
-        print("zoom:",zoom)
-        print("grid_zoom:",grid_zoom)
-        print("grid_number:",grid_number)
+        if grid_number > 65536:
+            grid_number = 65536*pow(2, findNearstPow2(zoom-tolerance_zoom-3))
+        print("zoom:", zoom)
+        print("grid_zoom:", grid_zoom)
+        print("grid_number:", grid_number)
         Count = pow(2, zoom * 2)
         print("Total tiles:", Count)
         print("===")
