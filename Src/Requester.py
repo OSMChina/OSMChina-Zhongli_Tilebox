62840f734f684c07cfb7caa38227c27a7204dd23
LaoshuBaby <keaitianxinmail@qq.com>
2021-11-27 12:22:43 +0800

    重构

--- Src/Requester.py
+++ Src/Requester.py
@@ -1,10 +1,8 @@
 import os
 import platform
-import time
-import json
 import random
 import threading
-from math import sqrt
+import time
 
 import requests
 
@@ -168,72 +166,3 @@ def multipleTask(x_min, x_max, y_min, y_max, z, tile_name, task_name, ALLOW_MP=F
 
     time_end = time.time()
     print("[TIME] " + task_name + ": " + str(time_end - time_start) + "s")
-
-
-def taskGenerator(zoom: int, tile_name, task_name, x_min=0, x_max=0, y_min=0, y_max=0,
-                  grid_pos=(0, 0), MODE="Region", ALLOW_MP=False):
-    # INIT
-    global WHITE_LIST
-    global TILE_SERVER
-    headers["User-Agent"] = UA()
-    if platform.system() == "Windows":
-        WHITE_LIST = json.loads(open('..\\Res\\control_list.json').read())[
-            'WHITE_LIST']
-        TILE_SERVER = json.loads(open('..\\Res\\tile_server.json').read())
-    else:
-        WHITE_LIST = json.loads(
-            open('../Res/control_list.json').read())['WHITE_LIST']
-        TILE_SERVER = json.loads(open('../Res/tile_server.json').read())
-    # TASK
-    if MODE == "Region":
-        if zoom == 0:
-            print("Error: zoom must be greater than 0 in Region MODE")
-        else:
-            if x_min == 0 and x_max == 0 and y_min == 0 and y_max == 0:
-                x_min = int(input("Please input x_min:"))
-                x_max = int(input("Please input x_max:"))
-                y_min = int(input("Please input y_min:"))
-                y_max = int(input("Please input y_max:"))
-            count = (x_max - x_min + 1) * (y_max - y_min + 1)
-            print("Total tiles:", count)
-            multipleTask(x_min, x_max, y_min, y_max, zoom,
-                         tile_name, task_name, ALLOW_MP)
-    elif MODE == "Full":
-        if zoom == 0:
-            count = 1
-            print("Total tiles:", count)
-            multipleTask(0, 0, 0, 0, 0, tile_name, task_name)
-        else:
-            count = pow(2, zoom * 2)
-            print("Total tiles:", count)
-            multipleTask(0, pow(2, zoom) - 1, 0, pow(2, zoom) -
-                         1, zoom, tile_name, task_name, ALLOW_MP)
-    elif MODE == "Grid":
-        def findNearstPow2(x: int):
-            for i in range(19):
-                if pow(2, i) >= x:
-                    return pow(2, i)
-
-        tolerance_zoom = 7
-        grid_zoom = int(pow(2, int(zoom - tolerance_zoom + 1))) // 2
-        if zoom >= 10:
-            grid_zoom = findNearstPow2(
-                int(sqrt(int(pow(3, int(zoom - tolerance_zoom + 1))) // 1.5)))
-        if grid_zoom <= 0:
-            grid_zoom = 0
-        if grid_zoom <= 1:
-            grid_number = int(pow(2, grid_zoom // 2))
-        elif zoom >= 10:
-            grid_number = int(pow(2, grid_zoom // 2))
-        else:
-            grid_number = int(pow(2, grid_zoom))
-        if grid_number > 65536:
-            grid_number = 65536 * pow(2, findNearstPow2(zoom - tolerance_zoom - 3))
-        print("zoom:", zoom)
-        print("grid_zoom:", grid_zoom)
-        print("grid_number:", grid_number)
-        count = pow(2, zoom * 2)
-        print("Total tiles:", count)
-        print("===")
-    else:
-        print("Error: MODE Error")
