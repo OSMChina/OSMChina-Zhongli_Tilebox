9b981ad07502311e7e60774632dcb1d0c4cfdc10
LaoshuBaby <keaitianxinmail@qq.com>
2021-12-06 16:42:32 +0800

    改用os.path生成路径
    避免手动根据platform来hack
    此外，json不可以用black来处理

--- src/config.py
+++ src/config.py
@@ -1,18 +1,11 @@
 import os
-import platform
 import json
 
 global WHITE_LIST
 global TILE_SERVER
-pwd=os.path.dirname(os.path.realpath(__file__))
-path_control_list=os.path.join(pwd,"..","res","control_list.json")
-if platform.system() == "Windows":
-    WHITE_LIST = json.loads(open(path_control_list).read())[
-        "WHITE_LIST"
-    ]
-    TILE_SERVER = json.loads(open("..\\res\\tile_server.json").read())
-else:
-    WHITE_LIST = json.loads(open("../res/control_list.json").read())[
-        "WHITE_LIST"
-    ]
-    TILE_SERVER = json.loads(open("../res/tile_server.json").read())
+
+pwd = os.path.dirname(os.path.realpath(__file__))
+path_control_list = os.path.join(pwd, "..", "res", "control_list.json")
+path_tile_server = os.path.join(pwd, "..", "res", "tile_server.json")
+WHITE_LIST = json.loads(open(path_control_list).read())["WHITE_LIST"]
+TILE_SERVER = json.loads(open(path_tile_server).read())
